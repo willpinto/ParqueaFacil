eddfa9227988b4162fec25922feb28e3727e39f9
Nib-RA <elniborson@gmail.com>
2021-06-25 09:52:18 -0500

    Ajuste de creación de BD

--- setup/crearprocediminto.php
+++ setup/crearprocediminto.php
@@ -4,26 +4,38 @@ require_once '../modelo/conexion.php';
 $data = array();
 $pdo = Conexion::Conectar();
 try {
-	$sql = " 
+	$sql = "
 		DROP PROCEDURE IF EXISTS `actualizaradministrador`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizaradministrador` (IN `cedula` INT, IN `nombres` VARCHAR(250), IN `cargo` VARCHAR(50), IN `contrasena` VARCHAR(50))  NO SQL
 		UPDATE administrador SET nombres = nombres, cargo = cargo, contrasena = contrasena WHERE documento = cedula;
-
+		
 		DROP PROCEDURE IF EXISTS `actualizarpropietario`;
-		CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizarpropietario` (IN `cedula` INT(10), IN `nombres` VARCHAR(100), IN `apellidos` VARCHAR(100), IN `direccion` VARCHAR(250), IN `telefono` VARCHAR(20), IN `correo` VARCHAR(100), IN `fnacimiento` DATE, IN `genero` CHAR(1), IN `tlicencia` VARCHAR(3), IN `nlicencia` VARCHAR(20))  NO SQL
-		UPDATE propietario_vehiculo SET nombres = nombres, apellidos = apellidos, direccion = direccion, telefono = telefono, correo = correo, fecha_nacimiento = fnacimiento, genero = genero, tipo_licencia = tlicencia, numero_licencia = nlicencia WHERE documento = cedula;
-
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizarpropietario` (IN `cedula` INT(10), IN `tdocumento` VARCHAR(2), IN `nombres` VARCHAR(100), IN `apellidos` VARCHAR(100), IN `direccion` VARCHAR(250), IN `telefono` VARCHAR(20), IN `correo` VARCHAR(100), IN `fnacimiento` DATE, IN `genero` CHAR(1), IN `tlicencia` VARCHAR(3), IN `nlicencia` VARCHAR(20))  NO SQL
+		UPDATE conductor SET tipo_documento = tdocumento, nombres = nombres, apellidos = apellidos, direccion = direccion, telefono = telefono, correo = correo, fecha_nacimiento = fnacimiento, genero = genero, tipo_licencia = tlicencia, numero_licencia = nlicencia WHERE documento = cedula;
+		
 		DROP PROCEDURE IF EXISTS `actualizarregistro`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizarregistro` (IN `id` INT(10), IN `fingreso` DATETIME, IN `fsalida` DATETIME, IN `estado` BOOLEAN, IN `placaveh` VARCHAR(7), IN `documentovig` INT(10))  NO SQL
-		UPDATE registro SET fecha_ingreso = fingreso, fecha_salida = fsalida, estado = estado, placa_veh = placaveh, documento_vig = documentovig WHERE numero_ticket = id;
-
+		UPDATE registro SET fecha_ingreso = fingreso, fecha_salida = fsalida, estado = estado, id_cond_veh = placaveh, documento_vig = documentovig WHERE numero_ticket = id;
+		
 		DROP PROCEDURE IF EXISTS `actualizarvehiculo`;
-		CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizarvehiculo` (IN `id` VARCHAR(7), IN `tipo` VARCHAR(10), IN `marca` VARCHAR(50), IN `linea` VARCHAR(50), IN `modelo` INT(4), IN `servicio` VARCHAR(20), IN `cilindraje` INT(6), IN `chasis` VARCHAR(20), IN `motor` VARCHAR(20), IN `color` VARCHAR(30), IN `tcarroceria` VARCHAR(30), IN `documentopro` INT(10))  NO SQL
-		UPDATE vehiculo SET tipo = tipo, marca = marca, linea = linea, modelo = modelo, servicio = servicio, cilindraje = cilindraje, chasis = chasis, motor = motor, color = color, tipo_carroceria = tcarroceria, documento_pro_veh = documentopro WHERE placa = id;
-
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizarvehiculo` (IN `id` VARCHAR(7), IN `tipo` VARCHAR(10), IN `marca` VARCHAR(50), IN `linea` VARCHAR(50), IN `modelo` INT(4), IN `servicio` VARCHAR(20), IN `cilindraje` INT(6), IN `chasis` VARCHAR(20), IN `motor` VARCHAR(20), IN `color` VARCHAR(30), IN `tcarroceria` VARCHAR(30))  NO SQL
+		UPDATE vehiculo SET tipo = tipo, marca = marca, linea = linea, modelo = modelo, servicio = servicio, cilindraje = cilindraje, chasis = chasis, motor = motor, color = color, tipo_carroceria = tcarroceria WHERE placa = id;
+		
 		DROP PROCEDURE IF EXISTS `actualizarvigilante`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizarvigilante` (IN `cedula` INT, IN `nombres` VARCHAR(50), IN `turno` VARCHAR(10), IN `rol` VARCHAR(25), IN `contrasena` VARCHAR(50), IN `documentoadm` INT)  NO SQL
 		UPDATE vigilante SET nombres = nombres, turno = turno, rol = rol, contrasena = contrasena WHERE documento = cedula;
+		
+		DROP PROCEDURE IF EXISTS `asociarconductorvehiculo`;
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `asociarconductorvehiculo` (IN `documento` INT(10), IN `placa` VARCHAR(7))  NO SQL
+		INSERT INTO conductor_vehiculo VALUES(null, documento, placa);
+		
+		DROP PROCEDURE IF EXISTS `cargarnovedadespornumeroticket`;
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `cargarnovedadespornumeroticket` (IN `id` INT)  NO SQL
+		SELECT n.tipo, n.descripcion FROM novedad_vehiculo n INNER JOIN registro r ON r.numero_ticket = n.numero_ticket_reg WHERE n.numero_ticket_reg = id AND r.estado = 1;
+		
+		DROP PROCEDURE IF EXISTS `cargarnovedadesporregistro`;
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `cargarnovedadesporregistro` (IN `id` INT)  NO SQL
+		SELECT * FROM novedad_vehiculo WHERE numero_ticket_reg = id;
 
 		DROP PROCEDURE IF EXISTS `consultaradministrador`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultaradministrador` (IN `id` INT)  NO SQL
@@ -41,9 +53,13 @@ try {
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarnumerovehiculosportipo` (IN `tipoveh` VARCHAR(10))  NO SQL
 		SELECT * FROM vehiculo WHERE tipo = tipoveh;
 
+		DROP PROCEDURE IF EXISTS `consultarplacasactivas`;
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarplacasactivas` ()  NO SQL
+		SELECT r.numero_ticket idRegistro, cv.placa_veh placa FROM registro r INNER JOIN conductor_vehiculo cv ON r.id_cond_veh = cv.id WHERE r.estado = 1;
+
 		DROP PROCEDURE IF EXISTS `consultarpropietario`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarpropietario` (IN `cedula` INT(10))  NO SQL
-		SELECT * FROM propietario_vehiculo WHERE documento = cedula;
+		SELECT * FROM conductor WHERE documento = cedula;
 
 		DROP PROCEDURE IF EXISTS `consultarregistro`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarregistro` (IN `id` INT(10))  NO SQL
@@ -51,11 +67,15 @@ try {
 
 		DROP PROCEDURE IF EXISTS `consultarregistroactivo`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarregistroactivo` (IN `cedula` INT(10), IN `placa` VARCHAR(7))  NO SQL
-		SELECT r.numero_ticket, pv.documento, pv.nombres, pv.apellidos, v.placa, v.tipo, v.marca, v.linea FROM registro r INNER JOIN vehiculo v ON r.placa_veh = v.placa INNER JOIN propietario_vehiculo pv ON v.documento_pro_veh = pv.documento WHERE pv.documento = cedula AND r.placa_veh = placa AND r.estado = 1;
+		SELECT r.numero_ticket, c.documento, c.nombres, c.apellidos, v.placa, v.tipo, v.marca, v.linea FROM registro r INNER JOIN conductor_vehiculo cv ON r.id_cond_veh = cv.id INNER JOIN vehiculo v ON cv.placa_veh = v.placa INNER JOIN conductor c ON cv.documento_cond = c.documento WHERE cv.documento_cond = cedula AND cv.placa_veh = placa AND r.estado = 1 ORDER BY r.numero_ticket DESC LIMIT 1;
 
 		DROP PROCEDURE IF EXISTS `consultarregistroactivoporplaca`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarregistroactivoporplaca` (IN `placa` VARCHAR(7))  NO SQL
-		SELECT * FROM registro WHERE placa_veh = placa AND estado = 1 ORDER BY numero_ticket DESC LIMIT 1;
+		SELECT r.* FROM registro r INNER JOIN conductor_vehiculo cv ON r.id_cond_veh = cv.id WHERE cv.placa_veh = placa AND r.estado = 1 ORDER BY r.numero_ticket DESC LIMIT 1;
+
+		DROP PROCEDURE IF EXISTS `consultarregistroactivoporticket`;
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarregistroactivoporticket` (IN `numero` INT(10))  NO SQL
+		SELECT r.* FROM registro r INNER JOIN conductor_vehiculo cv ON r.id_cond_veh = cv.id WHERE r.numero_ticket = numero AND r.estado = 1 ORDER BY r.numero_ticket DESC LIMIT 1;
 
 		DROP PROCEDURE IF EXISTS `consultarvehiculo`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarvehiculo` (IN `id` VARCHAR(7))  NO SQL
@@ -63,19 +83,19 @@ try {
 
 		DROP PROCEDURE IF EXISTS `consultarvehiculoporplacadocumento`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarvehiculoporplacadocumento` (IN `placa` VARCHAR(7), IN `documento` INT)  NO SQL
-		SELECT * FROM vehiculo WHERE placa = placa AND documento_pro_veh = documento;
+		SELECT v.* FROM vehiculo v INNER JOIN conductor_vehiculo cv ON cv.placa_veh = v.placa WHERE v.placa = placa AND cv.documento_cond = documento;
 
 		DROP PROCEDURE IF EXISTS `consultarvehiculosegresadosentrefechas`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarvehiculosegresadosentrefechas` (IN `finicio` DATE, IN `ffinal` DATE)  NO SQL
-		SELECT v.* FROM vehiculo v INNER JOIN registro r ON v.placa = r.placa_veh WHERE r.fecha_salida BETWEEN finicio AND ffinal;
+		SELECT cv.documento_cond conductor, v.placa, v.tipo, v.marca, v.linea, v.modelo, r.numero_ticket id_registro, r.fecha_salida FROM vehiculo v INNER JOIN conductor_vehiculo cv ON cv.placa_veh = v.placa INNER JOIN registro r ON cv.id = r.id_cond_veh WHERE r.fecha_salida BETWEEN finicio AND DATE_ADD(ffinal, INTERVAL 1 DAY);
 
 		DROP PROCEDURE IF EXISTS `consultarvehiculosingresadosentrefechas`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarvehiculosingresadosentrefechas` (IN `finicio` DATE, IN `ffinal` DATE)  NO SQL
-		SELECT v.* FROM vehiculo v INNER JOIN registro r ON v.placa = r.placa_veh WHERE r.fecha_ingreso BETWEEN finicio AND ffinal;
+		SELECT cv.documento_cond conductor, v.placa, v.tipo, v.marca, v.linea, v.modelo, r.numero_ticket id_registro, r.fecha_ingreso FROM vehiculo v INNER JOIN conductor_vehiculo cv ON cv.placa_veh = v.placa INNER JOIN registro r ON cv.id = r.id_cond_veh WHERE r.fecha_ingreso BETWEEN finicio AND  DATE_ADD(ffinal, INTERVAL 1 DAY);
 
 		DROP PROCEDURE IF EXISTS `consultarvehiculosporpropietario`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarvehiculosporpropietario` (IN `documento` INT)  NO SQL
-		SELECT * FROM vehiculo WHERE documento_pro_veh = documento;
+		SELECT v.* FROM vehiculo v INNER JOIN conductor_vehiculo cv ON cv.placa_veh = v.placa WHERE cv.documento_cond = documento;
 
 		DROP PROCEDURE IF EXISTS `consultarvigilante`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `consultarvigilante` (IN `id` INT)  NO SQL
@@ -87,7 +107,7 @@ try {
 
 		DROP PROCEDURE IF EXISTS `eliminarpropietario`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `eliminarpropietario` (IN `cedula` INT(10))  NO SQL
-		DELETE FROM propietario_vehiculo WHERE documento = cedula;
+		DELETE FROM conductor WHERE documento = cedula;
 
 		DROP PROCEDURE IF EXISTS `eliminarregistro`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `eliminarregistro` (IN `id` INT(10))  NO SQL
@@ -107,7 +127,7 @@ try {
 
 		DROP PROCEDURE IF EXISTS `listarpropietarios`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `listarpropietarios` ()  NO SQL
-		SELECT * FROM propietario_vehiculo;
+		SELECT * FROM conductor;
 
 		DROP PROCEDURE IF EXISTS `listarregistros`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `listarregistros` ()  NO SQL
@@ -121,30 +141,41 @@ try {
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `listarvigilantes` ()  NO SQL
 		SELECT * FROM vigilante;
 
+		DROP PROCEDURE IF EXISTS `obtenerDatosConductorCheckOut`;
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `obtenerDatosConductorCheckOut` (IN `id` INT(10))  NO SQL
+		SELECT c.documento, c.nombres, c.apellidos, v.placa FROM registro r INNER JOIN conductor_vehiculo cv ON r.id_cond_veh = cv.id INNER JOIN conductor c ON cv.documento_cond = c.documento INNER JOIN vehiculo v ON cv.placa_veh = v.placa WHERE r.numero_ticket = id ORDER BY id DESC LIMIT 1;
+
+		DROP PROCEDURE IF EXISTS `obteneridasociadoconductorvehiculo`;
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `obteneridasociadoconductorvehiculo` (IN `documento` INT(10), IN `placa` VARCHAR(7))  NO SQL
+		SELECT id FROM conductor_vehiculo WHERE documento_cond = documento AND placa_veh = placa ORDER BY id DESC LIMIT 1;
+
 		DROP PROCEDURE IF EXISTS `obteneridregistroactivoporplaca`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `obteneridregistroactivoporplaca` (IN `placa` VARCHAR(7))  NO SQL
-		SELECT numero_ticket FROM registro WHERE placa_veh = placa AND estado = 1 ORDER BY numero_ticket DESC LIMIT 1;
+		SELECT r.numero_ticket FROM registro r INNER JOIN conductor_vehiculo cv ON r.id_cond_veh = cv.id WHERE cv.placa_veh = placa AND r.estado = 1 ORDER BY r.numero_ticket DESC LIMIT 1;
 
 		DROP PROCEDURE IF EXISTS `registraradministrador`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `registraradministrador` (IN `cedula` INT, IN `nombres` VARCHAR(250), IN `cargo` VARCHAR(50), IN `contrasena` VARCHAR(50))  NO SQL
 		INSERT INTO administrador VALUES(cedula, nombres, cargo, contrasena);
 
+		DROP PROCEDURE IF EXISTS `registrarnovedad`;
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `registrarnovedad` (IN `tipo` VARCHAR(25), IN `descripcion` TEXT, IN `ticket` INT(11))  NO SQL
+		INSERT INTO novedad_vehiculo VALUES(null, tipo, descripcion, ticket);
+
 		DROP PROCEDURE IF EXISTS `registrarpropietario`;
-		CREATE DEFINER=`root`@`localhost` PROCEDURE `registrarpropietario` (IN `documento` INT(10), IN `nombres` VARCHAR(100), IN `apellidos` VARCHAR(100), IN `direccion` VARCHAR(250), IN `telefono` VARCHAR(20), IN `correo` VARCHAR(100), IN `fnacimiento` DATE, IN `genero` CHAR(1), IN `tlicencia` VARCHAR(3), IN `nlicencia` VARCHAR(20))  NO SQL
-		INSERT INTO propietario_vehiculo VALUES(documento, nombres, apellidos, direccion, telefono, correo, fnacimiento, genero, tlicencia, nlicencia);
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `registrarpropietario` (IN `documento` INT(10), IN `tdocumento` VARCHAR(2), IN `nombres` VARCHAR(100), IN `apellidos` VARCHAR(100), IN `direccion` VARCHAR(250), IN `telefono` VARCHAR(20), IN `correo` VARCHAR(100), IN `fnacimiento` DATE, IN `genero` CHAR(1), IN `tlicencia` VARCHAR(3), IN `nlicencia` VARCHAR(20))  NO SQL
+		INSERT INTO conductor VALUES(documento, tdocumento, nombres, apellidos, direccion, telefono, correo, fnacimiento, genero, tlicencia, nlicencia);
 
 		DROP PROCEDURE IF EXISTS `registrarregistro`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `registrarregistro` (IN `fingreso` DATETIME, IN `fsalida` DATETIME, IN `estado` BOOLEAN, IN `placaveh` VARCHAR(7), IN `documentovig` INT(10))  NO SQL
 		INSERT INTO registro VALUES(null, fingreso, fsalida, estado, placaveh, documentovig);
 
 		DROP PROCEDURE IF EXISTS `registrarvehiculo`;
-		CREATE DEFINER=`root`@`localhost` PROCEDURE `registrarvehiculo` (IN `placa` VARCHAR(7), IN `tipo` VARCHAR(10), IN `marca` VARCHAR(50), IN `linea` VARCHAR(50), IN `modelo` INT(4), IN `servicio` VARCHAR(20), IN `cilindraje` INT(6), IN `chasis` VARCHAR(20), IN `motor` VARCHAR(20), IN `color` VARCHAR(30), IN `tcarroceria` VARCHAR(30), IN `documentopro` INT(10))  NO SQL
-		INSERT INTO vehiculo VALUES(placa, tipo, marca, linea, modelo, servicio, cilindraje, chasis, motor, color, tcarroceria, documentopro);
+		CREATE DEFINER=`root`@`localhost` PROCEDURE `registrarvehiculo` (IN `placa` VARCHAR(7), IN `tipo` VARCHAR(10), IN `marca` VARCHAR(50), IN `linea` VARCHAR(50), IN `modelo` INT(4), IN `servicio` VARCHAR(20), IN `cilindraje` INT(6), IN `chasis` VARCHAR(20), IN `motor` VARCHAR(20), IN `color` VARCHAR(30), IN `tcarroceria` VARCHAR(30))  NO SQL
+		INSERT INTO vehiculo VALUES(placa, tipo, marca, linea, modelo, servicio, cilindraje, chasis, motor, color, tcarroceria);
 
 		DROP PROCEDURE IF EXISTS `registrarvigilante`;
 		CREATE DEFINER=`root`@`localhost` PROCEDURE `registrarvigilante` (IN `cedula` INT, IN `nombres` VARCHAR(50), IN `turno` VARCHAR(10), IN `rol` VARCHAR(25), IN `contrasena` VARCHAR(50), IN `documentoadm` INT)  NO SQL
 		INSERT INTO vigilante VALUES(cedula, nombres, turno, rol, contrasena, documentoadm);";
-
 	$comando = $pdo->exec($sql);
 	$data['status'] = 'ok';
 } catch (PDOException $e) {
--- setup/creartablas.php
+++ setup/creartablas.php
@@ -7,14 +7,16 @@ try {
 		CREATE TABLE administrador(documento INT PRIMARY KEY, nombres VARCHAR(50) NOT NULL, cargo VARCHAR(30) NOT NULL, contrasena VARCHAR(50) NOT NULL);
 	DROP TABLE IF EXISTS vigilante;
 		CREATE TABLE vigilante(documento INT PRIMARY KEY, nombres VARCHAR(50) NOT NULL, turno VARCHAR(10) NOT NULL, rol VARCHAR(25) NOT NULL, contrasena VARCHAR(50) NOT NULL, documento_adm INT NOT NULL, FOREIGN KEY(documento_adm) REFERENCES ADMINISTRADOR(documento));
-	DROP TABLE IF EXISTS propietario_vehiculo;
-		CREATE TABLE propietario_vehiculo(documento INT PRIMARY KEY, nombres VARCHAR(50) NOT NULL, apellidos VARCHAR(50) NOT NULL, direccion VARCHAR(150) NOT NULL, telefono VARCHAR(25) NULL, correo VARCHAR(50) NULL, fecha_nacimiento DATETIME NOT NULL, genero CHAR NOT NULL, tipo_licencia VARCHAR(2) NOT NULL, numero_licencia VARCHAR(20) NOT NULL);
+	DROP TABLE IF EXISTS conductor;
+		CREATE TABLE conductor(documento INT PRIMARY KEY, tipo_documento VARCHAR(2) NOT NULL, nombres VARCHAR(50) NOT NULL, apellidos VARCHAR(50) NOT NULL, direccion VARCHAR(150) NOT NULL, telefono VARCHAR(25) NULL, correo VARCHAR(50) NULL, fecha_nacimiento DATE NOT NULL, genero CHAR(1) NOT NULL, tipo_licencia VARCHAR(2) NOT NULL, numero_licencia VARCHAR(20) NOT NULL);
 	DROP TABLE IF EXISTS vehiculo;
-		CREATE TABLE vehiculo(placa VARCHAR(7) PRIMARY KEY, tipo VARCHAR(20) NOT NULL, marca VARCHAR(25) NOT NULL, linea VARCHAR(20) NOT NULL, modelo SMALLINT NOT NULL, servicio VARCHAR(20) NOT NULL, cilindraje TINYINT NOT NULL, chasis VARCHAR(20) NULL, motor VARCHAR(20) NULL, color VARCHAR(25) NOT NULL, tipo_carroceria VARCHAR(20) NULL, documento_pro_veh INT NOT NULL, FOREIGN KEY(documento_pro_veh) REFERENCES PROPIETARIO_VEHICULO(documento));
+		CREATE TABLE vehiculo(placa VARCHAR(7) PRIMARY KEY, tipo VARCHAR(20) NOT NULL, marca VARCHAR(25) NOT NULL, linea VARCHAR(20) NOT NULL, modelo SMALLINT NOT NULL, servicio VARCHAR(20) NOT NULL, cilindraje SMALLINT(5) NOT NULL, chasis VARCHAR(20) NULL, motor VARCHAR(20) NULL, color VARCHAR(25) NOT NULL, tipo_carroceria VARCHAR(20) NULL);
+	DROP TABLE IF EXISTS conductor_vehiculo;
+		CREATE TABLE conductor_vehiculo(id INT AUTO_INCREMENT PRIMARY KEY, documento_cond INT NOT NULL, placa_veh VARCHAR(7) NOT NULL, FOREIGN KEY(documento_cond) REFERENCES CONDUCTOR(documento), FOREIGN KEY(placa_veh) REFERENCES VEHICULO(placa));
 	DROP TABLE IF EXISTS registro;
-		CREATE TABLE registro(numero_ticket INT AUTO_INCREMENT PRIMARY KEY, fecha_ingreso DATETIME NOT NULL, fecha_salida DATETIME NOT NULL, estado BIT NOT NULL, placa_veh VARCHAR(7) NOT NULL, documento_vig INT NOT NULL, FOREIGN KEY(placa_veh) REFERENCES VEHICULO(placa), FOREIGN KEY(documento_vig) REFERENCES VIGILANTE(documento));
+		CREATE TABLE registro(numero_ticket INT AUTO_INCREMENT PRIMARY KEY, fecha_ingreso DATETIME NOT NULL, fecha_salida DATETIME NOT NULL, estado BIT(1) NOT NULL, id_cond_veh INT NOT NULL, documento_vig INT NOT NULL, FOREIGN KEY(id_cond_veh) REFERENCES CONDUCTOR_VEHICULO(id), FOREIGN KEY(documento_vig) REFERENCES VIGILANTE(documento));
 	DROP TABLE IF EXISTS noverdad_vehiculo;
-		CREATE TABLE novedad_vehiculo(id INT AUTO_INCREMENT PRIMARY KEY, tipo VARCHAR(25) NOT NULL, descripcion TEXT NULL, numero_ticket_reg INT NOT NULL, FOREIGN KEY(numero_ticket_reg) REFERENCES REGISTRO(numero_ticket));";
+		CREATE TABLE noverdad_vehiculo(id INT AUTO_INCREMENT PRIMARY KEY, tipo VARCHAR(25) NOT NULL, descripcion VARCHAR(250) DEFAULT NULL, numero_ticket_reg INT NOT NULL, FOREIGN KEY(numero_ticket_reg) REFERENCES REGISTRO(numero_ticket));";
 	$comando = $pdo->exec($sql);
 	$data['status'] = 'ok';
 } catch (PDOException $e) {
